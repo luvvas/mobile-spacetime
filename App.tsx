61c3a6a1990ee0fff21db43f7e2da0bc6bfce70b
luvvas <lucasmachado1@ufpr.br>
2023-05-20 17:16:22 -0300

    feat: add fonts config in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) return null
+
   return (
     <View className="flex-1 items-center justify-center bg-zinc-900">
-      <Text className="text-5xl text-gray-50">Teste</Text>
+      <Text className="text-5xl text-gray-50 font-alt">Teste</Text>
       <StatusBar style="light" translucent />
     </View>
   )
