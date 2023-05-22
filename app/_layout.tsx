6c7c9642b9a7aebf0fed1ca61e556386a620826d
luvvas <lucasmachado1@ufpr.br>
2023-05-22 08:57:58 -0300

    feat: add authentication redirect

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { ImageBackground } from 'react-native'
 import { SplashScreen, Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import * as SecureStore from 'expo-secure-store'
 import { styled } from 'nativewind'
 
 import {
@@ -13,15 +14,26 @@ import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
 import blurBg from '../assets/bg-blur.png'
 import Stripes from '../assets/stripes.svg'
+import { useEffect, useState } from 'react'
 
 const StyledStripes = styled(Stripes)
 
 export default function Layout() {
+  const [isUserAuthenticated, setisUserAuthenticated] = useState<
+    null | boolean
+  >(null)
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) => {
+      setisUserAuthenticated(!!token)
+    })
+  }, [])
 
   if (!hasLoadedFonts) return <SplashScreen />
 
@@ -39,7 +51,12 @@ export default function Layout() {
           headerShown: false,
           contentStyle: { backgroundColor: 'transparent' },
         }}
-      />
+      >
+        {/* redirect redireciona para a próxima rota caso o valor
+        do estado isUserAuthenticated seja true */}
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="memories" />
+      </Stack>
     </ImageBackground>
   )
 }
